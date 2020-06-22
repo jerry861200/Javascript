076537727f9c4f987b29176633636dee4b0b29ba
jerry861200 <37608370+jerry861200@users.noreply.github.com>
2020-06-22 18:20:00 +0800

    fianl

--- TodoList/todolist/src/App.vue
+++ TodoList/todolist/src/App.vue
@@ -1,32 +1,37 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/all">全部</router-link> |
+      <router-link to="/active">未完成</router-link> |
+      <router-link to="/complete">已完成</router-link>
     </div>
-    <router-view/>
+    <TodoInput/>
+    <div id="list">
+      <TodoListItem v-for="index in todoIndex" :key="index" :index="index"/>
+    </div>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style src="./assets/style.css">
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
+
+<script>
+import TodoInput from '@/components/TodoInput/TodoInput/Todo/index.vue'
+import TodoListItem from '@/components/TodoListItem/index.vue'
+
+export default {
+  components:{
+    TodoInput,
+    TodoListItem
+  },
+  computed:{
+    todoIndex(){
+      return this.$store.getters['todoIndex']
+    }
+  },
+  mounted(){
+    this.$store.dispatch('INIT_TODOS')
+  }
+}
+</script>  
